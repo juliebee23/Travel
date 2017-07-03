a1307087eb8c65ce1224cd0bd7a04ed39a84531f
Julie <jevans6363@gmail.com>
2017-07-03 19:07:58 -0400

    Made some more changes.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,9 +2,11 @@ class WelcomeController < ApplicationController
   def index
     @homeland = 'United States of America'
     @countries = ['Iceland','Norway','Ireland','Thailand']
-    @country_images = ['iceland.jpg','']
+    @country_images = ['iceland.jpg','Norway.jpg','ireland.jpg','Thailand.jpg']
   end
 
-  def params
+  def about_params
+    @color = params[:color]
+    @size = params[:shoe_size]
   end
 end
