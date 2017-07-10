3b8ad7f9bb56ad5b1d4deae12c718d15679b2267
Julie <jevans6363@gmail.com>
2017-07-10 19:17:20 -0400

    Finished up project. Now its time for my own modifications...

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170705223041) do
+ActiveRecord::Schema.define(version: 20170710223527) do
 
   create_table "destinations", force: :cascade do |t|
     t.string "city"
@@ -18,6 +18,11 @@ ActiveRecord::Schema.define(version: 20170705223041) do
     t.text "description"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "population"
+    t.string "avatar_file_name"
+    t.string "avatar_content_type"
+    t.integer "avatar_file_size"
+    t.datetime "avatar_updated_at"
   end
 
 end
